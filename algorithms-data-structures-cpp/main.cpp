772b569137e27638b13f3b9b3a68f6c545091bcb
Bakare Emmanuel <bakare@deimos.co.za>
2019-08-12 04:19:40 +0100

    Init with simple hello world program

---
+++ algorithms-data-structures-cpp/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  algorithms-data-structures-cpp
+//
+//  Created by mac on 11/07/2019.
+//  Copyright © 2019 mac. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
